60a6a468efb0b9f69e1afef73d6e5940bc9a12d7
HAlexTM <halexercraft@gmail.com>
2017-09-02 18:24:14 +0200

    Add background to inputs if the helper class "bg-*" is used, fix white table

---
+++ index.js
@@ -0,0 +1,32 @@
+var cookie = getCookie("bg-color");
+if (cookie !== "") {
+    changeTheme(cookie);
+}
+
+function changeTheme(name) {
+    document.body.className = "container-boxed " + (name == "white" ? "theme-red styled-scrollbar-colored" : "styled-scrollbar") + " bg-" + name;
+    setCookie("bg-color", name, 60);
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
